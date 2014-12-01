f7cce3d3482c16e35e9395298aa48f2c226463b5
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2014-12-01 19:09:01 +0200

    History, step 5
     - added visited label

--- Resources/public/js/app/models/history/history-model.js
+++ Resources/public/js/app/models/history/history-model.js
@@ -3,14 +3,25 @@ define(function (require) {
     'use strict';
 
     var HistoryModel,
-        BaseModel = require('oroui/js/app/models/base/model');
+        BaseModel = require('oroui/js/app/models/base/model'),
+        datetimeFormatter = require('orolocale/js/formatter/datetime');
 
     HistoryModel = BaseModel.extend({
         defaults: {
             url: '',
             title: '',
             visits: 1,
-            visited: null
+            visited: null,
+            formatted: null
+        },
+
+        initialize: function () {
+            this.on('add change:visited', this.formatVisited);
+        },
+
+        formatVisited: function () {
+            var formatted = datetimeFormatter.formatDateTime(this.get('visited'));
+            this.set('formatted', formatted, {silent: true});
         }
     });
 
